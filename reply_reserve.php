348e1c3c6439ab91fa9dbdeeeba687e53bba5a3d
abigail826 <65316661+abigail826@users.noreply.github.com>
2020-06-13 19:05:39 -0600

    Add files via upload

---
+++ reply_reserve.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >HOTEL FLORENCIA</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+		<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">Sobre nosotros</a></li> |
+		<li><a href = "contactus.php">Contactanos</a></li> |
+		<li><a href = "gallery.php">Galeria</a></li> |
+		<li><a href = "dineandlounge.php">cena y salón</a></li> |			
+		<li><a href = "reservation.php">Reserva</a></li> |
+		<li><a href = "rulesandregulation.php">Reglas</a></li>
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>Realiza una reservacion</h3></strong>
+				<br />
+				<div class = "col-md-4"></div>
+				<div class = "well col-md-4">
+					<center><h3>
+Visite nuestro hotel para verificar</h3></center>
+					<br />
+					<center><h4>GRACIAS!</h4></center>
+					<br />
+					<center><a href = "reservation.php" class = "btn btn-success"><i class = "glphyicon glyphicon-check"></i> volver a la reserva</a></center>
+				</div>
+				<div class = "col-md-4"></div>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
