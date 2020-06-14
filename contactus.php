66c5140d5304a27cf3a24063b74be964d9cf119b
Marbeli10 <65312479+Marbeli10@users.noreply.github.com>
2020-06-13 20:34:38 -0500

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >HOTEL FLORENCIA</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+		<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">Sobre nosotros</a></li> |
+		<li><a href = "contactus.php">Contactanos</a></li> |
+		<li><a href = "gallery.php">Galeria</a></li> |
+		<li><a href = "dineandlounge.php">cena y salón</a></li> |			
+		<li><a href = "reservation.php">Reserva</a></li> |
+		<li><a href = "rulesandregulation.php">Reglas</a></li>
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>CONTACTANOS</h3></strong>
+				<br />
+				<br />
+				<center><img src = "images/logo.JPEG" width = "300" height = "300" /></center>
+				<br />
+				<br />
+				<center>
+				<p>Contactano al:+50379008361</p>
+				<p>jc6768257@gmail.com</p>
+				</center>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
---
+++ rulesandregulation.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div
+		class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >HOTEL FLORENCIA</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+			<li><a href = "aboutus.php">Sobre nosotros</a></li> |
+		<li><a href = "contactus.php">Contactanos</a></li> |
+		<li><a href = "gallery.php">Galeria</a></li> |
+		<li><a href = "dineandlounge.php">cena y salón</a></li> |			
+		<li><a href = "reservation.php">Reserva</a></li> |
+		<li><a href = "rulesandregulation.php">Reglas</a></li>
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>RULES AND REGULATION</h3></strong>
+				<br />
+				<strong><h4 style = "color:#ff0000;">1.Tarifa</h4></strong>
+				<p>La tarifa es solo para la habitación y no incluye impuestos gubernamentales. Las comidas y otros servicios están disponibles a un costo adicional. Para conocer la tarifa de su habitación, comuníquese con el gerente de servicio, los formularios de registro de invitados deben estar firmados a la llegada.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">2.Liquidación de facturas</h4></strong>
+				<p>Las facturas deben liquidarse en la presentación, no se aceptan cheques personales.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">3. Gravamen de la compañía sobre el equipaje y pertenencias de los huéspedes</h4></strong>
+				<p>En caso de incumplimiento en el pago de las cuotas por parte de un invitado, la gerencia tendrá la ropa en su equipaje y pertenencias, y tendrá derecho a detenerla y vender o subastar dicha propiedad en cualquier momento sin referencia al huésped. Los ingresos de la venta neta serán apropiados para el monto adeudado por el huésped sin perjuicio de los derechos de la gerencia para adoptar los procedimientos de recuperación adicionales que se requieran.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">4.Check-in</h4></strong>
+				<p>Presente su tarjeta de identificación, pasaporte o tarjeta de residencia temporal al momento del check-in. Por ley, los visitantes deben presentar documentos personales para los registros del hotel. Estos documentos serán devueltos a la salida.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">5. partida</h4></strong>
+				<p>La hora de salida es (mencione su hora de salida aquí), informe a la reciepción si desea conservar su habitación más allá de este tiempo. La extensón se otorgará según la disponibilidad. Si la habitación está disponible, se cobrará la tarifa normal. En caso de que el huésped deje la habitación al vencimiento o período, la gerencia tendrá derecho a retirar al huésped y sus pertenencias de la habitación ocupada por el huésped.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">6.equipaje y almacenamiento</h4></strong>
+				<p>Sujeto a la disponibilidad del espacio de almacenamiento, el huésped puede almacenar el equipaje en la sala de equipajes, bajo su exclusivo riesgo como pérdida o daño por cualquier causa. El equipaje no puede almacenarse por un período de más de 30 días.
+
+</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">7.mascotas</h4></strong>
+				<p>Mencione su política para mascotas (permitidas o no permitidas) (Permítanos hacer arreglos por separado).
+
+.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">8. Hazardous Goods</h4></strong>
+				<p>Bringing goods and / or storing of raw or exposed cinema films, or any other article of a combustible or hazardous nature and / or prohibited goods and / or goods of objectionable nature is prohibited.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">9.  Bienes peligrosos</h4></strong>
+				<p>Está prohibido traer bienes y / o almacenar películas de cine en bruto o expuestas, o cualquier otro artículo de naturaleza combustible o peligrosa y / o bienes prohibidos y / o bienes de naturaleza objetable.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">10. Daño a la propiedad</h4></strong>
+				<p>El huésped será responsable de cualquier pérdida o daño a la propiedad del hotel causado por ellos mismos, sus invitados o cualquier persona de la que sean responsables.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">11. Relación entre la gerencia y el huésped</h4></strong>
+				<p>Nada de lo mencionado anteriormente continuará o se considerará que constituye, o creará un arrendamiento o sub-arrendamiento, o cualquier otro derecho a interactuar en las instalaciones del hotel o cualquier parte de la misma, a favor de cualquier Huésped o residente o visitante, y la Administración siempre se considerará en posesión total y absoluta de la totalidad de las instalaciones del hotel.</p>
+				<br />
+				<strong><h4 style = "color:#ff0000;">12. Reglas y regulaciones gubernamentales y aplicación de leyes.</h4></strong>
+				<p>Se solicita a los huéspedes que observen, cumplan con la confirmación y estén sujetos a todos los actos y leyes aplicables y las normas y reglamentos gubernamentales vigentes de vez en cuando..</p>
+				<br />
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
