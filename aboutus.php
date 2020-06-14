f2d75166edad96268e99af072e86cc00d293b59d
walter3108 <49923420+walter3108@users.noreply.github.com>
2020-06-13 19:01:54 -0600

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Hotel Florencia</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+		<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">Sobre nosotros</a></li> |
+		<li><a href = "contactus.php">Contactanos</a></li> |
+		<li><a href = "gallery.php">Galeria</a></li> |
+		<li><a href = "dineandlounge.php">cena y salón</a></li> |			
+		<li><a href = "reservation.php">Reserva</a></li> |
+		<li><a href = "rulesandregulation.php">Reglas</a></li>
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>Sobre Nosotros</h3></strong>
+				<p style = "position:relative; float:left; width:850px;">El hotel presenta un célebre equilibrio de nostalgia y estilo contemporáneo, capturando su elegancia, lujo y decadencia originales del sur. Los azulejos de Machuca forman patrones geométricos geniales en los pasillos. Los pisos de madera noble del hotel contrastan con muebles modernos y comodidades en las espectaculares suites. El estilo de vida del hotel ofrece a los huéspedes las mejores indulgencias sensoriales: artículos de tocador orgánicos relajantes, recetas tradicionales y privacidad y tranquilidad inigualables.</p>
+				<img style = "float:right;" src = "images/about.jpg" width = "250px" height = "250px" />
+				<br style = "clear:both;" />
+				<br />
+				<br />
+				<hr style = "border:1px dotted #000;" />
+				<br />
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/1.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Standard</h4></center>
+					<center><label>Cuarto pequeño</label> <label style = "color:red;">2,000.00</label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/2.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Cama Extra</h4></center>
+					<center><label style = "color:red;">800.00</label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/3.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Superior</h4></center>
+					<center><label>una cama mediana</label> <label style = "color:red;">2,400.00 </label></center>
+				</div>
+				<br style = "clear:both;"/>
+				<br />
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/4.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Super de lujo</h4></center>
+					<center><label>2 camas y medias </label> <label style = "color:red;">2,800.00 </label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/5.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">Jr. Suite</h4></center>
+					<center><label>Matrimonial</label> <label style = "color:red;">3,800.00 </label></center>
+				</div>
+				<div style = "float:left; margin-left:40px; width:300px; height:300px; ">
+					<center><img src = "images/6.jpg" width = "250px" height = "250px"  style = "margin-top:5px;"/></center>
+					<center><h4 style = "color:rgba(0, 255, 0, 1);">suite esclusiva</h4></center>
+					<center><label>Matrimonial</label> <label style = "color:red;">4,000.00 </label></center>
+				</div>
+				<br style = "clear:both;"/>
+				<br />
+				<br />
+				<strong><h3>Comodidades y servicios
+</h3></strong>
+				<ul>
+					<li><label>servicio de habitaciones las 24 horas</label></li>
+					<li><label>Televisor de pantalla plana de 21 "con servicio de cable</label></li>
+					<li><label>Ducha caliente y fría</label></li>
+					<li><label>Nevera y minibar a pedido en todas las suites y habitaciones superiores.</label></li>
+					<li><label>Juego de café y té, agua embotellada, toldos orgánicos en suites y habitaciones superiores.</label></li>
+					<li><label>Secador de pelo en habitaciones suite</label></li>
+					<li><label>Cajas de seguridad personal en cada habitación.</label></li>
+					<li><label>Lavandería y prensado</label></li>
+					<li><label>Wifi gratis</label></li>
+					<li><label>Servicio de masajes en la habitación.</label></li>
+					<li><label>Caja fuerte personal en cada habitación</label></li>
+					<li><label>Mini Bar</label></li>
+					<li><label>7 salas de reuniones y funciones</label></li>
+					<li><label>Viaje por carretera Traslados al aeropuerto y recorrido especial por la ciudad</label></li>
+					<li><label>Piscina</label></li>
+					<li><label>Tienda de regalos</label></li>
+					<li><label>Centro de negocios</label></li>
+					<li><label>Aparcamiento gratuito para invitados</label></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
