adba69e512a48f1a7075bc62f196eff351b845a6
ivania1012 <65377760+ivania1012@users.noreply.github.com>
2020-06-13 22:48:57 -0600

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >HOTEL FLORENCIA</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+	<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">Sobre nosotros</a></li> |
+		<li><a href = "contactus.php">Contactanos</a></li> |
+		<li><a href = "gallery.php">Galeria</a></li> |
+		<li><a href = "dineandlounge.php">cena y salón</a></li> |			
+		<li><a href = "reservation.php">Reserva</a></li> |
+		<li><a href = "rulesandregulation.php">Reglas</a></li>	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>GALERIA</h3></strong>
+				<br />
+				<br />
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/1.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/2.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/3.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/4.jpg" width = "250" height = "250"/>
+				</div>
+				<br style = "clear:both;" />
+				<br />
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/5.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/6.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/7.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/8.jpg" width = "250" height = "250"/>
+				</div>
+				<br style = "clear:both;" />
+				<br />
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/9.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/10.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/11.jpg" width = "250" height = "250"/>
+				</div>
+				<div style = "float:left; width:250px; height:250px; margin-left:20px;">
+					<img src = "images/gallery/12.jpg" width = "250" height = "250"/>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
